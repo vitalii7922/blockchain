c806ecb4bb125792c7d8c618d3c2097c000cf3c5
vitalii7922 <nefedovvi792@gmail.com>
2020-07-03 23:44:28 +0300

    commit

--- Blockchain/task/src/blockchain/BlockElementFactory.java
+++
@@ -1,9 +0,0 @@
-package blockchain;
-
-
-import java.time.LocalTime;
-
-public interface BlockElementFactory {
-    public Block makeBlock(int id, long timeStamp, int magicNumber, String currentBlockHash,
-                           long timeGenerating);
-}
